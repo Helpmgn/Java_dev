96933ed0158685ba1a91b974ad1c47fe1fe34f3f
helpmgn <helpmgn@yandex.ru>
2017-04-10 16:12:43 +0500

    Create Calc

--- Calculator.java
+++ Calculator.java
@@ -10,11 +10,48 @@
 	 суммируем аргументы.
 	 @param params Аргументы суммирования.
 	 */
-	 public void add (int ... params) {
-		 for (Integer param : params) {
-			 this.result += param;
-		 }
+	 public void summ (int x, int y) {
+		 this.result = x+y;
+		 System.out.println("Summa : " + this.result);
 	 }
+	 public void summ_buff(int ... params) {
+		 for (Integer param : params) {
+            this.result += param;
+        }
+		 System.out.println("Summa : " + this.result);
+	 }
+	 /**
+	 вычитаем аргументы.
+	 @param params Аргументы вычитания.
+	 */
+	 public void razn (int x, int y) {
+		 this.result = x-y;
+		 System.out.println("Raznost : " + this.result);
+	 }
+	 /**
+	 вычитаем аргументы.
+	 @param params Аргументы умножения.
+	 */
+	 public void proizv (int x, int y) {
+		 this.result = x*y;
+		 System.out.println("Proizvedenie : " + this.result);
+	 }
+	 /**
+	 вычитаем аргументы.
+	 @param params Аргументы деления.
+	 */
+	 public void del (int x, int y) {
+		 this.result = x/y;
+		 System.out.println("Chastnoe : " + this.result);
+	 }
+	 /**
+	 вычитаем аргументы.
+	 @param params Аргументы степени.
+	 
+	 public void summ (int x, int y) {
+		 this.result = x+y;
+		 System.out.println("Summa : " + this.result);
+	 }*/
 	 /**
 	 получить результат.
 	 @return результат вычисления.
--- InteructRunner.java
+++ InteructRunner.java
@@ -11,14 +11,77 @@ public class InteructRunner {
 		try {
 			Calculator calc = new Calculator();
 			String exit = "no";
+			float buff = 0;
+			//Integer Rez = 0;
 			while (!exit.equals("yes")) {
 				System.out.println("Enter first arg : ");
 				String first = reader.next(); // next() - vozvrashaet nam stroku.
 				System.out.println("Enter second arg : ");
 				String second = reader.next();
-				calc.add(Integer.valueOf(first), Integer.valueOf(second));
-				System.out.println("Exit : yes/no ");
-				exit = reader.next();
+				System.out.println("Enter operation (+,-,*,/) : ");
+				String opr = reader.next();
+				if (buff!=0) {
+					switch(opr) {
+						case "+":
+							calc.summ_buff(Integer.valueOf(first), Integer.valueOf(second));
+						//	Rez = Rez  + calc.getResult();
+						//	System.out.println("Summa + sohranenniy rezultat : " + Rez);
+						break;
+						case "-":
+							calc.razn(Integer.valueOf(first), Integer.valueOf(second));
+						break;
+						case "*":
+							calc.proizv(Integer.valueOf(first), Integer.valueOf(second));
+						break;
+						case "/":
+							calc.del(Integer.valueOf(first), Integer.valueOf(second));
+						break;
+					}
+					System.out.println("Save result? y/n");
+					String save = reader.next();
+					switch (save) {
+						case "y":
+							buff = 1;
+						//	Rez = calc.getResult();
+						break;
+						case "n":
+							buff = 0;
+							//calc.cleanResult();
+						break;
+					}
+					System.out.println("Exit : yes/no ");
+					exit = reader.next();
+				}
+				if (buff==0) {
+					switch(opr) {
+						case "+":
+							calc.summ(Integer.valueOf(first), Integer.valueOf(second));
+						break;
+						case "-":
+							calc.razn(Integer.valueOf(first), Integer.valueOf(second));
+						break;
+						case "*":
+							calc.proizv(Integer.valueOf(first), Integer.valueOf(second));
+						break;
+						case "/":
+							calc.del(Integer.valueOf(first), Integer.valueOf(second));
+						break;
+					}
+					System.out.println("Save result? y/n");
+					String save = reader.next();
+					switch (save) {
+						case "y":
+							buff = 1;
+					//		Rez = calc.getResult();
+						break;
+						case "n":
+							buff = 0;
+							//calc.cleanResult();
+						break;
+					}
+					System.out.println("Exit : yes/no ");
+					exit = reader.next();
+				}
 			}
 		} finally {
 			reader.close();
